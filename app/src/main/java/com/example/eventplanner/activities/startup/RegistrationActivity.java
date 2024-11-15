4a0e785926016330feac257cb50b86f50b15785b
unknown <kovacevicbranka123@gmail.com>
2024-11-15 20:48:20 +0100

    [ADD] EO registration page design

--- app/src/main/java/com/example/eventplanner/activities/startup/RegistrationActivity.java
+++ app/src/main/java/com/example/eventplanner/activities/startup/RegistrationActivity.java
@@ -3,6 +3,7 @@ package com.example.eventplanner.activities.startup;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -10,29 +11,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
 
 import com.example.eventplanner.R;
+import com.example.eventplanner.fragments.startup.EoRegistrationFragment;
+import com.example.eventplanner.helpers.FragmentsTool;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegistrationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_registration);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        TextView btn1 = findViewById(R.id.switchToRegister);
-        btn1.setOnClickListener(new View.OnClickListener() {
+
+        Button btnEOreg = findViewById(R.id.btnEO);
+        btnEOreg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
-
+                FragmentsTool.to(new EoRegistrationFragment(), RegistrationActivity.this);
             }
         });
-
     }
 }
