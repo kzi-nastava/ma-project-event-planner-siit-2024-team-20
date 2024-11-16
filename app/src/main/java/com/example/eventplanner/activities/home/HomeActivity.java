b5d06687be783709ba7de0f1068c11d9b7ab0737
andjelavostic <andjelavostic@gmail.com>
2024-11-16 02:28:05 +0100

    [ADD] sort menu drop down

--- app/src/main/java/com/example/eventplanner/activities/home/HomeActivity.java
+++ app/src/main/java/com/example/eventplanner/activities/home/HomeActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -72,5 +73,38 @@ public class HomeActivity extends AppCompatActivity {
             intent.putExtra("product_id", "1");  // Dodaj sve potrebne podatke kao extra
             startActivity(intent);
         });
+        ImageView sortButton = findViewById(R.id.sort_events);
+
+        // Poziv funkcije za prikazivanje Popup menija
+        sortButton.setOnClickListener(v -> showSortMenu(v));
+
     }
+    // Funkcija za prikazivanje menija
+    private void showSortMenu(View anchorView) {
+        // Kreiranje popup menija
+        PopupMenu popupMenu = new PopupMenu(this, anchorView);
+        getMenuInflater().inflate(R.menu.sort_menu, popupMenu.getMenu());
+
+        // Prikazivanje menija ispod dugmeta
+        popupMenu.show();
+
+        // Postavljanje logike za selektovanje
+        popupMenu.setOnMenuItemClickListener(item -> {
+            if (item.getItemId() == R.id.option_sort_asc) {
+                item.setChecked(true);
+                popupMenu.getMenu().findItem(R.id.option_sort_desc).setChecked(false);
+            } else if (item.getItemId() == R.id.option_sort_desc) {
+                item.setChecked(true);
+                popupMenu.getMenu().findItem(R.id.option_sort_asc).setChecked(false);
+            } else if (item.getItemId() == R.id.option_check_1) {
+                item.setChecked(!item.isChecked());
+            } else if (item.getItemId() == R.id.option_check_2) {
+                item.setChecked(!item.isChecked());
+            } else if (item.getItemId() == R.id.option_check_3) {
+                item.setChecked(!item.isChecked());
+            }
+            return true;
+        });
+    }
+
 }
