9d70ff03e9a1a99dde731ff818484c8121c55e89
unknown <kovacevicbranka123@gmail.com>
2025-02-09 12:56:17 +0100

    [IMPROVE] registration

--- app/src/main/java/com/example/eventplanner/fragments/startup/EoRegistrationFragment.java
+++ app/src/main/java/com/example/eventplanner/fragments/startup/EoRegistrationFragment.java
@@ -3,80 +3,104 @@ package com.example.eventplanner.fragments.startup;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.eventplanner.R;
 import com.example.eventplanner.activities.startup.LoginActivity;
-import com.example.eventplanner.activities.startup.RegistrationActivity;
+import com.example.eventplanner.model.registration.EoRegistrationRequest;
+import com.example.eventplanner.model.registration.EoRegistrationResponse;
+import com.example.eventplanner.services.IUserService;
+import com.example.eventplanner.services.spec.ApiService;
+import com.example.eventplanner.services.spec.RetrofitClient;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EoRegistrationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class EoRegistrationFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private EditText nameField, lastNameField, emailField, passwordField, confirmPasswordField, cityField, addressField, addressNumField, phoneNumberField;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public EoRegistrationFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EoRegistrationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EoRegistrationFragment newInstance(String param1, String param2) {
-        EoRegistrationFragment fragment = new EoRegistrationFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_eo_registration, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_eo_registration, container, false);
-        TextView btn1 = view.findViewById(R.id.switchToLogIn);
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), LoginActivity.class);
-                startActivity(intent);
-            }
-        });
+        nameField = view.findViewById(R.id.editTextText);
+        lastNameField = view.findViewById(R.id.editTextText2);
+        emailField = view.findViewById(R.id.editTextTextEmailAddress);
+        passwordField = view.findViewById(R.id.editTextTextPassword);
+        confirmPasswordField = view.findViewById(R.id.editTextTextPassword2);
+        cityField = view.findViewById(R.id.editTextText5);
+        addressField = view.findViewById(R.id.editTextText3);
+        addressNumField = view.findViewById(R.id.editTextText4);
+        phoneNumberField = view.findViewById(R.id.editTextPhone);
+
+        Button registerButton = view.findViewById(R.id.btnregister);
+        registerButton.setOnClickListener(v -> registerEventOrganizer());
         return view;
     }
+    private void registerEventOrganizer(){
+        String name = nameField.getText().toString().trim();
+        String lastName = lastNameField.getText().toString().trim();
+        String email = emailField.getText().toString().trim();
+        String password = passwordField.getText().toString();
+        String confirmPassword = confirmPasswordField.getText().toString();
+        String city = cityField.getText().toString().trim();
+        String address = addressField.getText().toString().trim();
+        String addressNum = addressNumField.getText().toString().trim();
+        String phoneNumber = phoneNumberField.getText().toString().trim();
+
+        if (name.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty() ||
+                confirmPassword.isEmpty() || city.isEmpty() || address.isEmpty() || addressNum.isEmpty() ||
+                phoneNumber.isEmpty()) {
+            Toast.makeText(getContext(), "All fields must bi filled.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (!password.equals(confirmPassword)) {
+            Toast.makeText(getContext(), "Passwords do not match.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(!isGmailAddress(email)){
+            Toast.makeText(getContext(), "Email must be valid gmail address.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        EoRegistrationRequest request = new EoRegistrationRequest(name, lastName, email, password, city, address, addressNum, phoneNumber);
+
+        IUserService userService = RetrofitClient.getClient(ApiService.BASE_URL)
+                .create(IUserService.class);
+        Call<EoRegistrationResponse> call = userService.eoRegistration(request);
+        call.enqueue(new Callback<EoRegistrationResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<EoRegistrationResponse> call, @NonNull Response<EoRegistrationResponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    Toast.makeText(getContext(), "Registration successful! Check your gmail to verify profile", Toast.LENGTH_SHORT).show();
+                    new android.os.Handler().postDelayed(() -> {
+                        Intent intent = new Intent(getActivity(), LoginActivity.class);
+                        startActivity(intent);
+                        requireActivity().finish();
+                    }, 3000);
+                } else {
+                    Toast.makeText(getContext(), "Registration error. Try again.", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<EoRegistrationResponse> call, @NonNull Throwable t) {
+                Toast.makeText(getContext(), "Error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), "Connection error, try again later.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private boolean isGmailAddress(String email) {
+        return email != null && email.endsWith("@gmail.com");
+    }
+
 }
